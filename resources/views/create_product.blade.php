624b44ebc573a487a71c5ebc6af1f385960c4ba2
DDARKWORK <kiforylaminekoneprofessional@gmail.com>
2022-03-13 20:15:57 +0000

    formulaire create_product (V2)

--- resources/views/create_product.blade.php
+++ resources/views/create_product.blade.php
@@ -12,23 +12,23 @@ Page de création de produits - We Fashion
 <div class="container" style="margin: 25px auto">
     <form action="" method="post" enctype="multipart/form-data">
         <div class="form-floating mb-3 form">
-            <input type="text" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <input type="text" class="form-control" id="floatingInput" placeholder="Entrez le nom du produit" name="name">
             <label for="floatingInput">Entrez le nom du produit</label>
         </div>
         <div class="form-floating" style="margin-bottom: 15px">
-            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
+            <textarea class="form-control" placeholder="Entrez la description du produit" id="floatingTextarea2" style="height: 100px" name="description"></textarea>
             <label for="floatingTextarea2">Entrez la description du produit</label>
         </div>
         <div class="form-floating form-floating mb-3">
-            <input type="text" class="form-control" id="floatingPassword" placeholder="Password">
+            <input type="text" class="form-control" id="floatingPassword" placeholder="Entrez la référence du produit" name="reference">
             <label for="floatingPassword">Entrez la référence du produit</label>
         </div>
         <div class="form-floating form-floating mb-3">
-            <input type="number" class="form-control" id="floatingPassword" placeholder="Password">
+            <input type="number" class="form-control" id="floatingPassword" placeholder="Entrez le prix du produit" name="price">
             <label for="floatingPassword">Entrez le prix du produit</label>
         </div>
         <div class="form-floating form-floating mb-3">
-            <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+            <select class="form-select" id="floatingSelect" aria-label="Floating label select example" name="condition">
                 <option selected disabled>Cliquez pour sélectionner l'état du produit</option>
               <option value="">Sold</option>
               <option value="">Standard</option>
@@ -36,7 +36,7 @@ Page de création de produits - We Fashion
             <label for="floatingSelect">Sélectionnez l'état du produit</label>
         </div>
         <div class="form-floating form-floating mb-3">
-            <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+            <select class="form-select" id="floatingSelect" aria-label="Floating label select example" name="visibility">
               <option selected disabled>Cliquez pour sélectionner la visibilité du produit</option>
               <option value="1">publié</option>
               <option value="2">non publié</option>
@@ -44,7 +44,7 @@ Page de création de produits - We Fashion
             <label for="floatingSelect">Sélectionnez la visibilité du produit</label>
         </div>
         
-        <input type="file" class="form-control" id="floatingPassword" placeholder="Password">
+        <input type="file" class="form-control" id="floatingPassword">
 </form>
 </div>
 </div>
